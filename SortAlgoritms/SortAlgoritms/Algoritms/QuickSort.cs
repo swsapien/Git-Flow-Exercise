af453b1e9c376ca0b8afcf08cefd2050950e6962
helttonr <heltton.ramirez@sw.com.mx>
2023-10-02 22:31:04 -0600

    no message

---
+++ SortAlgoritms/SortAlgoritms/Algoritms/QuickSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgoritms.Algoritms
+{
+    /// <summary>
+    /// Al igual que Merge Sort, Quick Sort utiliza la técnica de "dividir y conquistar". 
+    /// Se elige un elemento pivote y se divide la lista en dos subconjuntos: elementos menores 
+    /// que el pivote y elementos mayores que el pivote. Luego, se ordenan los subconjuntos y se combinan. 
+    /// Es eficiente y a menudo más rápido que Merge Sort en la práctica.
+    /// </summary>
+    public class QuickSort : IAlgoritm
+    {
+        public int[] GetDisorder(int numberValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] Sort(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validation(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
