f25a423fe734c102f438647c95ed8ba7a25392f7
Rich Barusta <ricardo.barusta@gmail.com>
2023-10-18 17:44:00 -0600

    se refactoriza countingSort

--- SortAlgoritms/SortAlgoritms/Algoritms/CountingSort.cs
+++ SortAlgoritms/SortAlgoritms/Algoritms/CountingSort.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SortAlgoritms.Algoritms
 {
     /// <summary>
@@ -17,17 +16,59 @@ namespace SortAlgoritms.Algoritms
     {
         public int[] GetDisorder(int numberValues)
         {
-            throw new NotImplementedException();
+            // Genera valores desordenados
+            Random random = new Random();
+            int[] values = new int[numberValues];
+            for (int i = 0; i < numberValues; i++)
+            {
+                values[i] = random.Next(numberValues); // Valores aleatorios dentro del rango
+            }
+            return values;
         }
-
         public int[] Sort(int[] values)
         {
-            throw new NotImplementedException();
+             if (values == null || values.Length <= 1)
+                return values;
+            int max = values[0];
+            int min = values[0];
+            // Encontrar el valor máximo y mínimo en el arreglo
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > max)
+                    max = values[i];
+                if (values[i] < min)
+                    min = values[i];
+            }
+            // Crear un arreglo de frecuencia para contar las ocurrencias de cada valor
+            int[] count = new int[max - min + 1];
+            // Contar las ocurrencias de cada valor
+            for (int i = 0; i < values.Length; i++)
+            {
+                count[values[i] - min]++;
+            }
+            // Reconstruir el arreglo ordenado
+            int index = 0;
+            for (int i = 0; i < count.Length; i++)
+            {
+                while (count[i] > 0)
+                {
+                    values[index] = i + min;
+                    index++;
+                    count[i]--;
+                }
+            }
+            return values;
         }
-
         public bool Validation(int[] values)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] < values[i - 1])
+                {
+                    return false; // El arreglo no está ordenado
+                }
+            }
+            return true; 
         }
     }
 }
