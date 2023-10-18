8f0ebab4bbf4a1479024665747c0fc24d8f6d694
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2023-10-18 17:25:24 -0600

    Delete comments

--- SortAlgoritms/SortAlgoritms/Algoritms/RadixSort.cs
+++ SortAlgoritms/SortAlgoritms/Algoritms/RadixSort.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace SortAlgorithms.Algorithms
+namespace SortAlgoritms.Algoritms
 {
     /// <summary>
     /// Radix Sort es útil para ordenar números enteros o cadenas alfabéticas. 
@@ -14,19 +14,16 @@ namespace SortAlgorithms.Algorithms
         {
             if (values == null || values.Length <= 1)
             {
-                // No es necesario ordenar
                 return values;
             }
 
-            // Encuentra el valor máximo para determinar el número de dígitos
             int max = values.Max();
-            int exp = 1; // Inicializa la posición del dígito menos significativo
+            int exp = 1;
 
-            // Hace el counting sort para cada posición del dígito
             while (max / exp > 0)
             {
                 CountingSort(values, exp);
-                exp *= 10; // Mueve a la siguiente posición del dígito
+                exp *= 10;
             }
 
             return values;
@@ -36,34 +33,29 @@ namespace SortAlgorithms.Algorithms
         {
             int n = values.Length;
             int[] output = new int[n];
-            int[] count = new int[10]; // Hay 10 dígitos posibles (0-9)
+            int[] count = new int[10];
 
-            // Inicializa el arreglo de conteo
             for (int i = 0; i < 10; i++)
             {
                 count[i] = 0;
             }
 
-            // Almacena la cuenta de ocurrencias de cada dígito
             for (int i = 0; i < n; i++)
             {
                 count[(values[i] / exp) % 10]++;
             }
 
-            // Ajusta el conteo para obtener la posición correcta de cada elemento
             for (int i = 1; i < 10; i++)
             {
                 count[i] += count[i - 1];
             }
 
-            // Construye el arreglo de salida
             for (int i = n - 1; i >= 0; i--)
             {
                 output[count[(values[i] / exp) % 10] - 1] = values[i];
                 count[(values[i] / exp) % 10]--;
             }
 
-            // Copia el arreglo de salida al arreglo original
             for (int i = 0; i < n; i++)
             {
                 values[i] = output[i];
